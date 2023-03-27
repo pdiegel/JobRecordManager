be76079f0141de1e50f1252887525a6b03f7012e
pdiegel <philipdiegel@gmail.com>
2023-03-27 08:37:18 -0400

    Added Tests

---
+++ tests/test_job_number_generator.py
@@ -0,0 +1,56 @@
+import unittest
+from datetime import datetime
+from helpers.job_number_storage import JobNumbers
+from helpers.job_number_generator import JobNumberGenerator
+
+
+class MockJobNumberStorage(JobNumbers):
+    def __init__(self, job_numbers=None):
+        self.job_numbers = job_numbers or []
+
+    def get_existing_job_numbers(self):
+        return self.job_numbers
+
+
+class TestJobNumberGenerator(unittest.TestCase):
+    def test_get_job_number_prefix(self):
+        job_storage = MockJobNumberStorage()
+        job_number_generator = JobNumberGenerator(job_storage)
+
+        now = datetime.now()
+        year = now.year % 100
+        month = now.month
+        expected_prefix = f"{year:02d}{month:02d}"
+
+        self.assertEqual(
+            job_number_generator.get_job_number_prefix(), expected_prefix
+        )
+
+    def test_unused_job_number(self):
+        now = datetime.now()
+        year = now.year % 100
+        month = now.month
+        existing_job_numbers = [
+            "11110511",
+            "23010105",
+            "16030171",
+            "23010111",
+            "05120933",
+            "22040237",
+            "93121259",
+            "22120530",
+            "12050267",
+        ]
+        job_storage = MockJobNumberStorage(existing_job_numbers)
+        job_number_generator = JobNumberGenerator(job_storage)
+
+        expected_job_number = f"23030112"
+        self.assertEqual(
+            job_number_generator.unused_job_number, expected_job_number
+        )
+
+        expected_job_number = f"{year:02d}{month:02d}0101"
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_job_number_storage.py
@@ -0,0 +1,63 @@
+import unittest
+from typing import List
+from helpers.job_number_storage import JobNumbers
+
+example_job_numbers = ["16030171", "23010111", "93121259"]
+
+
+class TestJobNumbers(unittest.TestCase):
+    def setUp(self):
+        self.job_numbers = JobNumbers()
+
+    def test_add_job_number(self):
+        self.job_numbers.add_job_number(example_job_numbers[0])
+        self.assertIn(
+            example_job_numbers[0], self.job_numbers.existing_job_numbers
+        )
+        self.assertIn(
+            example_job_numbers[0], self.job_numbers.active_job_numbers
+        )
+
+    def test_remove_job_number(self):
+        self.job_numbers.add_job_number(example_job_numbers[0])
+        self.job_numbers.remove_job_number(example_job_numbers[0])
+        self.assertNotIn(
+            example_job_numbers[0], self.job_numbers.existing_job_numbers
+        )
+
+    def test_add_existing_job_numbers(self):
+        existing_job_numbers = [
+            example_job_numbers[0],
+            example_job_numbers[1],
+            example_job_numbers[2],
+        ]
+        self.job_numbers.add_existing_job_numbers(existing_job_numbers)
+        self.assertTrue(
+            self.job_numbers.existing_job_numbers.issuperset(
+                existing_job_numbers
+            )
+        )
+
+    def test_add_active_job_numbers(self):
+        active_job_numbers = [
+            example_job_numbers[0],
+            example_job_numbers[1],
+            example_job_numbers[2],
+        ]
+        self.job_numbers.add_active_job_numbers(active_job_numbers)
+        self.assertTrue(
+            self.job_numbers.active_job_numbers.issuperset(active_job_numbers)
+        )
+
+    def test_clear_job_numbers(self):
+        self.job_numbers.add_existing_job_numbers(
+            [example_job_numbers[0], example_job_numbers[1]]
+        )
+        self.job_numbers.add_active_job_numbers([example_job_numbers[0]])
+        self.job_numbers.clear_job_numbers()
+        self.assertEqual(len(self.job_numbers.existing_job_numbers), 0)
+        self.assertEqual(len(self.job_numbers.active_job_numbers), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
